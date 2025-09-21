026cf594d969d4769d651663760b58dd89144761
McOriginal <originalmc223@gmail.com>
2025-09-21 23:20:38 +0000

    Ajoute de Login et Signin Screen

---
+++ lib/data/models/user_model.dart
@@ -0,0 +1,13 @@
+class UserModel {
+  final String fullName;
+  final String companyName;
+  final String adresse;
+  final int phoneNumer;
+
+  UserModel({
+    required this.fullName,
+    required this.companyName,
+    required this.adresse,
+    required this.phoneNumer,
+  });
+}
